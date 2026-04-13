dabb2dde1263e4f26cba5e39ed10fcf0505f8820
Carolline Ramos <carollineramosc@gmail.com>
2026-04-13 09:26:11 -0300

    Add error handling and invalid option function

--- app.py
+++ app.py
@@ -13,19 +13,30 @@ def finalizar_app():
     os.system('cls')
     print('Encerrando o app')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
+def opcao_invalida():
+    print('Opção inválida!')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
 
-    if opcao_escolhida == 1:
-        print('Cadastrar Restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar Restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar Restaurante')
-    else:
-        finalizar_app()
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+
+        if opcao_escolhida == 1:
+            print('Cadastrar Restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar Restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar Restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
