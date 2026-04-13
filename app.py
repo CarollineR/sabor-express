697d398954265d49ce75c207827c242af42df158
Carolline Ramos <carollineramosc@gmail.com>
2026-04-13 16:29:30 -0300

    Implement restaurant list
    Added a function to list registered restaurants and updated input prompts for clarity.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def finalizar_app():
 
 def opcao_invalida():
     print('Opção inválida!')
-    input('Digite uma tecla para voltar ao menu principal')
+    input('\nDigite uma tecla para voltar ao menu principal')
     main()
 
 def cadastrar_novo_restaurante():
@@ -26,8 +26,15 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal')
+    input('\nDigite uma tecla para voltar ao menu principal')
     main()
+
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes')
+
+for restaurante in restaurantes: 
+    print('f.{restaurante}')
 
 def escolher_opcao():
     try:
@@ -36,7 +43,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar Restaurante')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar Restaurante')
         elif opcao_escolhida == 4:
