79a8ad2838eed017d3997d1926814869c409285c
Carolline Ramos <carollineramosc@gmail.com>
2026-04-13 16:02:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os 
 
+restaurantes = []
+
 def exibir_nome_do_programa():
     print('Sᴀʙᴏʀ Exᴘʀᴇss')
 
@@ -18,12 +20,21 @@ def opcao_invalida():
     input('Digite uma tecla para voltar ao menu principal')
     main()
 
+def cadastrar_novo_restaurante():
+    os.system('cls')
+    print('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
 
         if opcao_escolhida == 1:
-            print('Cadastrar Restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
             print('Listar Restaurante')
         elif opcao_escolhida == 3:
